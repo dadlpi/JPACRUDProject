8adacb34f1617d4df8683a547cc06c7a3082b835
diego.delapuente35 <diego.a.delapuente@gmail.com>
2023-12-01 16:52:28 -0800

    create boot/MVC project, connect JPA dependency

---
+++ AutoMaintenance/src/main/java/com/skilldistillery/jpaautomantenance/AutoMaintenanceApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jpaautomantenance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoMaintenanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoMaintenanceApplication.class, args);
+	}
+
+}
---
+++ AutoMaintenance/src/test/java/com/skilldistillery/jpaautomantenance/AutoMaintenanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jpaautomantenance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoMaintenanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
